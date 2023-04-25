b6c3c9153ad7d1740dbb7b8b9315d50f37f3e682
Isaac Newton Dzikum <nisaacdz@gmail.com>
2023-04-25 20:20:48 +0000

    added EncryptedVote

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,29 @@
-use std::any::Any;
+use blockify::trans::record::Record;
+use serde::{Serialize, Deserialize};
 
-use blockify::{dist::Entity, record::Record, GenID};
-use serde::{Deserialize, Serialize};
+pub struct VoterId {}
+pub struct CandidateId {}
+pub struct ElectionId {}
 
-#[derive(Clone, Serialize, Deserialize)]
-pub struct VoteSession {}
-
-#[derive(Clone, Serialize, Deserialize)]
-struct Vote {
-    session: VoteSession,
-    choice: GenID,
-    public_key: Vec<u8>,
+#[derive(Serialize, Deserialize, Record)]
+pub struct EncryptedVote {
+    
 }
 
-impl Record for Vote {
-    fn is_valid(&self) -> bool {
-        true
-    }
-
-    fn get_signer(&self) -> &[u8] {
-        &self.public_key
-    }
+pub struct Vote {
+    voter: VoterId,
+    election: ElectionId,
+    choice: CandidateId,
 }
 
-pub trait Detail {
-    fn title(&self) -> &str;
-    fn body(&self) -> &dyn Any;
+pub stuct Voter {
+    id: VoterId,
+}
+pub struct Candidate {
+    id: CandidateId,
 }
 
-pub struct Voter {
-    pub details: Vec<Box<dyn Detail>>,
-    pub public_key: Vec<u8>,
-}
-
-impl Voter {
-    pub fn public_key(&self) -> &[u8] {
-        &self.public_key
-    }
-
-    pub fn get_detail<T: 'static>(&self, title: &str) -> Option<&T> {
-        for detail in self.details.iter() {
-            if detail.title() == title {
-                if let Some(body) = detail.body().downcast_ref::<T>() {
-                    return Some(body);
-                }
-            }
-        }
-        None
-    }
+pub struct Election {
+    id: ElectionId,
+    candidates: Vec<CandidateId>,
 }
